0ee6f6b20d730739c95182bc6c9e1aa73acc3b27
byungrock <obh7326@naver.com>
2024-01-12 03:06:04 +0900

    Youre

---
+++ src/component/FireBase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDg4EEnRQNyhWmALfFBkZujGBN9JzPXmN8",
+  authDomain: "healthgem-pro.firebaseapp.com",
+  projectId: "healthgem-pro",
+  storageBucket: "healthgem-pro.appspot.com",
+  messagingSenderId: "104497741323",
+  appId: "1:104497741323:web:abb6d01740fd806eaf510e",
+  measurementId: "G-P2EZ9B9LS1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// 실험
+export{app}
